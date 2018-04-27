d24dbf2a08650af54e32a291aca7c529e9393b68
zhangyongfeng <zhangyongfeng@davdian.com>
2018-04-27 18:06:49 +0800

    创建组件

---
+++ Example/Pods/Target Support Files/DVDZYLibTest/DVDZYLibTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DVDZYLibTest : NSObject
+@end
+@implementation PodsDummy_DVDZYLibTest
+@end
---
+++ Example/Pods/Target Support Files/Pods-DVDZYLibTest_Example/Pods-DVDZYLibTest_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DVDZYLibTest_Example : NSObject
+@end
+@implementation PodsDummy_Pods_DVDZYLibTest_Example
+@end
